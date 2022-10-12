1b90741e7bb20e87905e1e4c1af8ae384c483bb1
Эльчин <elcin03@mail.ru>
2022-10-12 20:47:10 +0300

    Task1

---
+++ src/ru/mirea/task1/ex1/task1.java
@@ -0,0 +1,20 @@
+package ru.mirea.task1.ex1;
+
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        int circle = 1;
+        while (circle == 1){
+            int sum = 0;
+            Scanner input = new Scanner(System.in);
+            System.out.print("Введите размер массива: ");
+            int n = input.nextInt();
+            for (int i = n; i != 0; i--) {
+                sum = sum + i;
+            }
+            System.out.println("сумма:" + sum + "\nостаться?(1-да, другое число-нет):");
+            circle = input.nextInt();
+        }
+    }
+}
